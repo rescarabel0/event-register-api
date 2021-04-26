e5a9b1e90d6facdcd5a5ae42378ca82246b96750
rescarabel0 <r.escarabelo@gmail.com>
2021-04-26 17:08:15 -0300

    Construtor

---
+++ app/entity/Evento.php
@@ -0,0 +1,89 @@
+<?php
+    namespace App\Entity;
+    class Evento{
+        
+        private $id;
+        private $titulo;
+        private $descricao;
+        private $data;
+        private $userId;
+        
+
+        //Construtor
+        public function __construct ($id = '', $titulo = '', $descricao = '', $data = '', $userId = ''){
+            $this->id = $id;
+            $this->titulo = $titulo;
+            $this->descricao = $descricao;
+            $this->data = $data;
+            $this->userId = $userId;
+        }
+        
+        //Setter & Getter 
+        public function getId()
+        {
+                return $this->id;
+        }
+        
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
+        }
+
+        //Setter & Getter 
+        public function getTitulo()
+        {
+                return $this->titulo;
+        }
+
+         
+        public function setTitulo($titulo)
+        {
+                $this->titulo = $titulo;
+
+                return $this;
+        }
+
+        //Setter & Getter 
+        public function getDescricao()
+        {
+                return $this->descricao;
+        }
+
+        
+        public function setDescricao($descricao)
+        {
+                $this->descricao = $descricao;
+
+                return $this;
+        }
+
+        //Setter & Getter 
+        public function getData()
+        {
+                return $this->data;
+        }
+
+        
+        public function setData($data)
+        {
+                $this->data = $data;
+
+                return $this;
+        }
+
+        //Setter & Getter 
+        public function getUserId()
+        {
+                return $this->userId;
+        }
+
+        public function setUserId($userId)
+        {
+                $this->userId = $userId;
+
+                return $this;
+        }
+
+    }
