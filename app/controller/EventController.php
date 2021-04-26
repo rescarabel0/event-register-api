fb87618155323d8368eae1bceb7e1c15e23690dd
rescarabel0 <r.escarabelo@gmail.com>
2021-04-26 20:03:02 -0300

    requisições do controller prontas

--- app/controller/EventController.php
+++ app/controller/EventController.php
@@ -6,7 +6,8 @@ use app\entity\Event;
 class EventController{
 
     function create($data = null){
-        return json_encode(["name" => "create"]);
+        $event = $this->convertDataToEventJson($data);
+        var_dump($event);
     }
 
     function update($data = null, $id = 0){
@@ -25,6 +26,14 @@ class EventController{
         return json_encode(["name" => "getall"]);
     }
 
-
+    private function convertDataToEventJson($data){
+        return new Event(
+            null,
+            (isset($data['titulo']) ? $data['titulo'] : null),
+            (isset($data['descricao']) ? $data['descricao'] : null),
+            (isset($data['data']) ? $data['data'] : null),
+            (isset($data['userId']) ? $data['userId'] : null),
+        );
+    }
     
 }
