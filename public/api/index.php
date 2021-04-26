4259de866cc891577c106e5a18b35bf16b1ac32d
rescarabel0 <r.escarabelo@gmail.com>
2021-04-26 18:19:37 -0300

    a

--- public/api/index.php
+++ public/api/index.php
@@ -4,8 +4,8 @@ require_once("../../vendor/autoload.php");
 // HEADER BASICS
 
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF=-8");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
