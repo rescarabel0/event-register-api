433e34429a7eb69f34121ef139516f3754e69231
rescarabel0 <r.escarabelo@gmail.com>
2021-04-27 13:09:41 -0300

    iniciando cadastro de usuarios

---
+++ public/db/CadastroUsuario.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+include('conection.php');
+
+$email = mysqli_real_escape_string($conexao, trim($_POST['email']));
+$usuario = mysqli_real_escape_string($conexao, trim($_POST['username']));
+$senha = mysqli_real_escape_string($conexao, trim(md5($_POST['password'])));
+
+$sql = "select count(*) as total from users where username = '$usuario'";
+$result = mysqli_query($conexao, $sql);
+$row = mysqli_fetch_assoc($result);
+
+if($row['total'] == 1) {
+    $_SESSION['usuario_existe'] = true;
+    header('Location: index.html');
+    exit();
+}
+
+$sql = "INSERT INTO users (email, username, password, data_cadastro) VALUES ('$email', '$usuario', '$senha', NOW())";
+
+if($conexao->query($sql) === TRUE) {
+    $_SESSION['cadastrado'] = true;
+}
+
+$conexao->close();
+
+header('Location: ../index.html');
+exit;
+?>
